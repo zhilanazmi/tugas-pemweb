0ba1475cbbfa1ffb2616dac06e90d94b7f30af88
Zhillan Azmi <jilaann@backboxindonesia.or.id>
2024-01-01 21:13:38 +0700

    menambah crud barang

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::controller(BarangController::class)->prefix('barang')->group( function () {
+    Route::get('', 'index')->name('barang');
+    Route::get('tambah', 'tambah')->name('barang.tambah');  
+    Route::post('tambah', 'simpan')->name('barang.tambah.simpan'); 
+    Route::get('edit/{id}', 'edit')->name('barang.edit');
+    Route::post('edit/{id}', 'update')->name('barang.tambah.update');  
+    Route::get('hapus/{id}', 'hapus')->name('barang.hapus');
 });
